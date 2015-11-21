703ed1af43ec960e83542c77a576bbd690027583
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-11-20 22:47:42 -0500

    updated

--- util.py
+++ util.py
@@ -1,3 +1,22 @@
 import google
 import requests
 import beautifulsoup
+import re
+
+def get_list_of_urls(to_google, num_results):
+    """
+    get_list_of_urls: returns a list of urls 
+
+    Args:
+        to_google (string): a string 
+        num_results (int): the number of results
+    
+    Returns:
+        a list of urls that the google api returns
+    
+    Example:
+        TODO
+    
+    Raises:
+        TODO
+    """
