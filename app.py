1a54c2f0463237df965fcde593d5c7dd5e2575f0
cdelano266 <kim.thunderbird@gmail.com>
2015-11-23 12:11:25 -0500

    working on results.html

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
+@app.route("/results")
+def results():
+    results = ['a', 'b', 'c']
+    return render_template('results.html', results)
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000, debug = True)
