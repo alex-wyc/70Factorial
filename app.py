110cda0ae8eb2919a8c0645fd077694567ff430a
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-11-28 20:15:19 -0500

    stubs added

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import util
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/")
 def root():
-    if request.method == "GET":
-        return render_template('index.html')
-    else:
-        query = request.form['sumName']
-        #results = 
-        return render_template('results.html')
+    return render_template("index.html")
+
+@app.route("/results", methods = ["GET", "POST"])
+def results():
+    # TODO this is where one uses backend for loading stuff
+    # NOTE that the method SHOULD BE GET
+    return
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000, debug = True)
